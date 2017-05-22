5d40333000008c939f3f4aba636dcd0c8e1849d7
jlbo <jlbueno@davinchi.es>
2017-05-22 15:29:04 +0200

    Bugs

---
+++ examples/course/sco1221/pages/6611/page.ts
@@ -0,0 +1,30 @@
+/**
+ * @license
+ * Copyright Davinchi. All Rights Reserved.
+ */
+import * as Prism "prismjs";
+import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-jade";
+import {PageFactory, PageRegister, PageController} from "@haztivity/core";
+import template from "./page.pug";
+import {HzDragtoboxResource} from "../../../resources/HzDragtobox";
+
+export let page: PageRegister = PageFactory.createPage(
+    {
+        name: "6611",
+        resources: [
+            HzDragtoboxResource
+        ],
+        template: template
+    }
+);
+page.on(
+    PageController.ON_SHOW, null, (eventObject, $page, $oldPage, oldPageRelativePosition, pageController) => {
+        Prism.highlightAll(false);
+    }
+);
+page.on(
+    PageController.ON_COMPLETE_CHANGE, null, (eventObject, $page, $oldPage, oldPageRelativePosition, pageController) => {
+        console.log("Completed change");
+    }
+);
---
+++ src/HzDragtoboxResource.ts
@@ -0,0 +1,92 @@
+/**
+ * @license
+ * Copyright Davinchi. All Rights Reserved.
+ */
+import {Resource,ResourceController,$,EventEmitterFactory,DataOptions} from "@haztivity/core";
+import "jquery-ui-dist/jquery-ui.js";
+import "jq-dragtobox";
+@Resource(
+    {
+        name:"HzDragtobox",
+        dependencies:[
+            $,
+            EventEmitterFactory,
+            DataOptions
+        ]
+    }
+)
+export class HzDragtoboxResource extends ResourceController {
+    protected static readonly DEFAULTS = {
+        
+    };
+    public static readonly NAMESPACE = "hzDragtobox";
+    protected _DataOptions:DataOptions;
+    protected _dragtoboxInstance:any;
+    protected _isOpen:boolean=false;
+    protected _id;
+    protected _namespace;
+
+    /**
+     * Recurso de tooltip para Haztivity.
+     * @param _$
+     * @param _EventEmitterFactory
+     * @param _ScormService
+     */
+    constructor(_$: JQueryStatic, _EventEmitterFactory, _DataOptions) {
+        super(_$, _EventEmitterFactory);
+        this._DataOptions = _DataOptions;
+    }
+
+    init(options, config?) {
+        this._config = config;
+        this._id = new Date().getTime();
+        this._namespace = HzDragtoboxResource.NAMESPACE + this._id;
+        this._options = options;
+        this.refresh();
+    }
+    public refresh(){
+        if(this._dragtoboxInstance){
+            this._dragtoboxInstance.destroy();
+        }
+        debugger;
+        let dragtoboxOptions = this._DataOptions.getDataOptions(this._$element, "hz-dragtobox");
+        this._options.dragtoboxOptions = this._$.extend(true,{},HzDragtoboxResource.DEFAULTS, dragtoboxOptions);
+        this._$element.dragtobox(this._options.dragtoboxOptions);
+        this._dragtoboxInstance = this._$element.data("dragtoboxModel");
+        this._assignEvents();
+    }
+
+    public getInstance(): any {
+        return this._dragtoboxInstance;
+    }
+    public disable(){
+        if(super.disable()){
+
+        }
+    }
+    public enable(){
+        if(super.enable()){
+
+        }
+    }
+
+    protected _assignEvents(){
+        this._$element.off("."+HzDragtoboxResource.NAMESPACE)
+            .one("dragtobox:completed."+HzDragtoboxResource.NAMESPACE,this._onResourceDone.bind(this))
+            .on("click."+HzDragtoboxResource.NAMESPACE+" hover."+HzDragtoboxResource.NAMESPACE,">*",this._onInteraction.bind(this));
+    }
+    protected _onInteraction(e){
+        if(this.isDisabled()){
+            e.stopPropagation();
+        }
+    }
+    protected _onResourceDone (e){
+        this._markAsCompleted();
+    }
+    public destroy(){
+        if(this._dragtoboxInstance) {
+
+        }
+        super.destroy();
+    }
+}
